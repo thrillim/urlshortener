65cd810a02396264e921121fb7a3bf682f6b7eaa
thrillim <86657389+thrillim@users.noreply.github.com>
2025-07-28 22:13:12 +0700

    fix vercel not_found error

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
 // Your first API endpoint
